c27d81f570b8fae7c9bf22e0c0cd2192d50e8819
AR1497 <av5938057@gmail.com>
2021-08-04 13:29:59 +0300

    Add Ex4

---
+++ lesson2/Ex4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Реализовать метод проверки логина и пароля.
+            //На вход метода подается логин и пароль.
+            //На выходе истина, если прошел авторизацию,
+            //и ложь, если не прошел
+            //(Логин: root, Password: GeekBrains).
+            //Используя метод проверки логина и пароля, написать программу:
+            //пользователь вводит логин и пароль,
+            //программа пропускает его дальше или не пропускает.
+            //С помощью цикла do while ограничить ввод пароля тремя попытками.
+            string login = "root";
+            string password = "GeekBrains";
+            
+            int k = 0;
+
+            do
+            {
+                Console.WriteLine("Введите логин: ");
+                var login_1 = Console.ReadLine();
+                Console.WriteLine("Введите пароль: ");
+                var password_1 = Console.ReadLine();
+
+                if ((login != login_1) || (password != password_1))
+                {
+                    Console.WriteLine("Неправильно введён пароль или логин");
+                }
+                else
+                {
+                    Console.WriteLine("Вы успешно вошли в учётную запись");
+                    break;
+                }
+                k++;
+            }
+            while (k < 3);
+        }
+    }
+}
