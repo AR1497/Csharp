d92d32ad14d7f920fc62f007a6ac43efc0bd9ef8
AR1497 <av5938057@gmail.com>
2021-08-08 19:16:57 +0300

    Add Ex2

---
+++ lesson3/Ex2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Summa
+    {
+        public int x = 1;
+        public int sum = 0;
+
+        public void Print()
+        {
+            Console.WriteLine("Введите числа: ");
+        }
+
+        //private static void Analysis()
+        //{
+        //    while (x > 0)
+        //    {
+        //        x = int.Parse(Console.ReadLine());
+
+        //        if ((x > 0) && (x % 2 == 1))
+        //        {
+        //            sum = sum + x;
+        //        }
+        //    }
+        //}
+
+    }
+
+    class Program
+    {
+        struct Analysis
+        {
+            public int An(int x, int sum)
+            {
+                while (x > 0) 
+                {
+                    x = int.Parse(Console.ReadLine());
+
+                    if ((x > 0) && (x % 2 == 1))
+                    {
+                        sum = sum + x;
+                    }
+                }
+                Console.WriteLine(sum);
+                return sum;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //а) С клавиатуры вводятся числа, пока не будет введён 0(каждое число в новой строке).
+            //Требуется подсчитать сумму всех нечётных положительных чисел.
+            //Сами числа и сумму вывести на экран, используя tryParse.
+
+            Summa summma1 = new Summa();
+            summma1.Print();
+            int x = 1;
+            int x1 = int.Parse(Console.ReadLine());
+            if (x != 0)
+            {
+                Analysis struct1;
+                struct1.An(1, 0);
+            }
+
+            //Console.WriteLine(summma1.sum);
+
+            //Console.WriteLine("Сумма нечётных положительных чисел: " + sum);
+
+        }
+    }
+}
